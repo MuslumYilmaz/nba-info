5f2554fd757184bd04ae72843f383893f0ea1fa3
MuslumYilmaz <muslum@vizyon.ai>
2021-05-31 21:31:32 +0300

    css active class for a tag

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@ const links = document.querySelectorAll('.scroll-to');
 
 links.forEach(item => {
     item.addEventListener('click', () => {
+        removeActiveState();
+        item.classList.add("active");
         const el = document.getElementById(item.getAttribute("data-link"));
         el.scrollIntoView({
             behavior: "smooth",
             block: "center"
         });
     });
-})
+});
+
+function removeActiveState() {
+    links.forEach(l => l.classList.remove("active"));
+}
